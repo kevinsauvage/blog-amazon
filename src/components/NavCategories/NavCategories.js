d7fe4b0d19fb1a4feec2b71dbe0ca6c6f8a66747
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-20 22:38:17 +0200

    create component

---
+++ src/components/NavCategories/NavCategories.js
@@ -0,0 +1,25 @@
+'use client';
+
+import Link from 'next/link';
+
+import Carousel from '../Carousel/Carousel';
+
+import styles from './NavCatagories.module.scss';
+
+const NavCategories = ({ categories }) => (
+  <nav className={styles.nav}>
+    <Carousel>
+      {categories.map((category) => (
+        <div
+          key={category.id}
+          className={styles.item}
+          style={{ backgroundColor: category.acf.background_color }}
+        >
+          <Link href={`/category/${category.slug}`}>{category.name}</Link>
+        </div>
+      ))}
+    </Carousel>
+  </nav>
+);
+
+export default NavCategories;
