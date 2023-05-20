6e672f46bd1a4bfbadd38b7133f95138ef2134f1
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-20 22:34:32 +0200

    update fetch and add categories

--- src/app/page.js
+++ src/app/page.js
@@ -2,41 +2,49 @@ import Link from 'next/link';
 
 import Container from '@/components/Container/Container';
 import Grid from '@/components/Grid/Grid';
+import NavCategories from '@/components/NavCategories/NavCategories';
 import Post from '@/components/Post/Post';
-import config from '@/config';
 import { formatPosts } from '@/utils/posts';
 
 import styles from './page.module.scss';
 
+const { WORDPRESS_API_URL } = process.env;
+
 const getPosts = async () => {
-  const response = await fetch(`${config.apiBaseUrl}/posts/?sticky=true`, {
-    next: { revalidate: 60 },
-  });
+  const URL = `${WORDPRESS_API_URL}/posts/?sticky=true&_embed`;
+  const response = await fetch(URL, { next: { revalidate: 60 } });
   const data = await response.json();
-  return formatPosts(data.posts);
+  return formatPosts(data);
+};
+
+const getCategories = async () => {
+  const URL = `${WORDPRESS_API_URL}/categories?acf_format=standard`;
+  const response = await fetch(URL, { next: { revalidate: 60 * 60 * 24 } });
+  return response.json();
 };
 
 const Home = async () => {
-  const posts = await getPosts();
+  const [posts, categories] = await Promise.all([getPosts(), getCategories()]);
 
   return (
     <main className={styles.main}>
       <Container>
+        <NavCategories categories={categories} />
         <ul className={styles.grid1}>
           {Array.isArray(posts) &&
-            posts.slice(0, 2).map((post) => (
-              <li key={post.ID}>
-                <Post post={post} aspect="video" />
-              </li>
-            ))}
+            posts
+              .slice(0, 2)
+              .map((post) => (
+                <Post key={post.ID} post={post} image={post.images.large} aspect="video" />
+              ))}
         </ul>
         <Grid>
           {Array.isArray(posts) &&
-            posts.slice(2).map((post) => (
-              <li key={post.ID}>
-                <Post post={post} aspect="square" />
-              </li>
-            ))}
+            posts
+              .slice(2)
+              .map((post) => (
+                <Post key={post.ID} post={post} image={post.images.medium_large} aspect="square" />
+              ))}
         </Grid>
         <div className={styles['see-all']}>
           <Link href="/archive">View all posts</Link>
