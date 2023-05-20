7f973f86d62b6a930a273b969d279db44487140e
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-20 22:36:23 +0200

    update fetch and remove li

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -4,27 +4,23 @@ import Pagination from '@/components/Pagination/Pagination';
 import Post from '@/components/Post/Post';
 import SearchForm from '@/components/SearchForm/SearchForm';
 import TotalFound from '@/components/TotalFound/TotalFound';
-import config from '@/config';
 import { formatPosts } from '@/utils/posts';
 
 import styles from './page.module.scss';
 
 const PER_PAGE = 9;
+const { WORDPRESS_API_URL } = process.env;
 
 const getSearch = async ({ searchParams }) => {
   const { q = '', page = 1 } = searchParams || {};
-
-  const response = await fetch(
-    `${config.apiBaseUrl}/posts?search=${q}&number=${PER_PAGE}&page=${page}`,
-    { next: { revalidate: 60 } }
-  );
-
+  const URL = `${WORDPRESS_API_URL}/posts?search=${q}&number=${PER_PAGE}&page=${page}&_embed`;
+  const response = await fetch(URL, { next: { revalidate: 60 } });
+  const totalPosts = response.headers.get('X-WP-Total');
   const data = await response.json();
-  const totalPosts = Number(data.found);
 
   return {
     currentPage: Number(page),
-    posts: formatPosts(data.posts),
+    posts: formatPosts(data),
     query: q,
     totalPages: Math.ceil(totalPosts / PER_PAGE),
     totalPosts,
@@ -38,16 +34,14 @@ const search = async (context) => {
     <Container>
       <main className={styles.main}>
         <header>
-          <h1 className={styles.title}>Search Results {query && `for ${query}`}</h1>
+          <h1>Search Results {query && `for ${query}`}</h1>
           <TotalFound total={totalPosts} />
           <SearchForm query={query} />
         </header>
         <Grid>
           {Array.isArray(posts) &&
             posts.map((post) => (
-              <li key={post.ID}>
-                <Post post={post} aspect="square" />
-              </li>
+              <Post key={post.ID} post={post} image={post.images.medium_large} aspect="square" />
             ))}
         </Grid>
         <Pagination totalPages={totalPages} currentPage={currentPage} />
