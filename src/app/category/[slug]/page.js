4fec885a53b549264084f0e25d46a7f296e7224d
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-20 22:35:29 +0200

    update fetch and remove li

--- src/app/category/[slug]/page.js
+++ src/app/category/[slug]/page.js
@@ -3,31 +3,26 @@ import Grid from '@/components/Grid/Grid';
 import Pagination from '@/components/Pagination/Pagination';
 import Post from '@/components/Post/Post';
 import TotalFound from '@/components/TotalFound/TotalFound';
-import config from '@/config';
 import { formatPosts } from '@/utils/posts';
 import { formatString } from '@/utils/strings';
 
 import styles from './page.module.scss';
 
 const PAGE_SIZE = 9;
+const { WORDPRESS_API_URL } = process.env;
 
 const getAllPosts = async ({ searchParams, params }) => {
   const { slug } = params;
-
   const { page = 1 } = searchParams || {};
-
-  const url = `${
-    config.apiBaseUrl
-  }/posts/?number=${PAGE_SIZE}&page=${page}&category=${encodeURIComponent(slug)}`;
-
+  const url = `${WORDPRESS_API_URL}/posts/?number=${PAGE_SIZE}&page=${page}&category=${slug}&_embed`;
   const response = await fetch(url, { next: { revalidate: 60 } });
+  const totalPosts = response.headers.get('X-WP-Total');
   const data = await response.json();
-  const totalPosts = Number(data.found);
   const totalPages = Math.ceil(totalPosts / PAGE_SIZE);
 
   return {
     currentPage: page,
-    posts: formatPosts(data.posts),
+    posts: formatPosts(data),
     totalPages,
     totalPosts,
   };
@@ -42,15 +37,13 @@ const archive = async (context) => {
     <Container>
       <main className={styles.main}>
         <header>
-          <h1 className={styles.title}>{formatString(slug)}</h1>
+          <h1>{formatString(slug)}</h1>
           <TotalFound total={totalPosts} />
         </header>
         <Grid>
           {Array.isArray(posts) &&
             posts.map((post) => (
-              <li key={post.ID}>
-                <Post post={post} aspect="square" />
-              </li>
+              <Post key={post.ID} post={post} image={post.images.medium_large} aspect="square" />
             ))}
         </Grid>
         <Pagination totalPages={totalPages} currentPage={currentPage} />
