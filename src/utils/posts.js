e397a4752b2130689de5bf501051675436bce158
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-20 22:39:21 +0200

    create format post func and use it

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -1,13 +1,16 @@
 // eslint-disable-next-line import/prefer-default-export
-export const formatPosts = (posts) =>
-  posts.map((post) => ({
-    ID: post.ID,
-    author: post.author,
-    categories: post.categories,
-    date: post.date,
-    excerpt: post.excerpt,
-    featured_image: post.featured_image,
-    slug: post.slug,
-    thumbnail: post.post_thumbnail,
-    title: post.title,
-  }));
+
+export const formatPost = (post) => ({
+  ID: post.id,
+  author: post.author,
+  categories: post._embedded?.['wp:term'][0],
+  content: post.content?.rendered,
+  date: post.date,
+  excerpt: post.excerpt?.rendered,
+  imageAlt: post._embedded?.['wp:featuredmedia']?.alt_text,
+  images: post._embedded?.['wp:featuredmedia'][0]?.media_details.sizes,
+  slug: post.slug,
+  title: post.title?.rendered,
+});
+
+export const formatPosts = (posts) => posts.map((post) => formatPost(post));
