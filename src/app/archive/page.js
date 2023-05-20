7afa3470ec6f93b7a594ce0c729c5cad2ee99d50
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-20 22:35:18 +0200

    update fetch and remove li

--- src/app/archive/page.js
+++ src/app/archive/page.js
@@ -3,27 +3,25 @@ import Grid from '@/components/Grid/Grid';
 import Pagination from '@/components/Pagination/Pagination';
 import Post from '@/components/Post/Post';
 import TotalFound from '@/components/TotalFound/TotalFound';
-import config from '@/config';
 import { formatPosts } from '@/utils/posts';
 
 import styles from './page.module.scss';
 
 const PAGE_SIZE = 9;
+const { WORDPRESS_API_URL } = process.env;
 
 const getAllPosts = async ({ searchParams }) => {
   const { page = 1 } = searchParams || {};
-
-  const response = await fetch(`${config.apiBaseUrl}/posts/?number=${PAGE_SIZE}&page=${page}`, {
-    next: { revalidate: 60 },
-  });
-
+  const URL = `${WORDPRESS_API_URL}/posts/?number=${PAGE_SIZE}&page=${page}&_embed`;
+  const response = await fetch(URL, { next: { revalidate: 60 } });
+  const totalPosts = response.headers.get('X-WP-Total');
   const data = await response.json();
 
   return {
     currentPage: page,
-    posts: formatPosts(data.posts),
-    totalPages: Math.ceil(Number(data.found) / PAGE_SIZE),
-    totalPosts: data.found,
+    posts: formatPosts(data),
+    totalPages: Math.ceil(Number(totalPosts) / PAGE_SIZE),
+    totalPosts,
   };
 };
 
@@ -34,15 +32,13 @@ const archive = async (context) => {
     <Container>
       <main className={styles.main}>
         <header>
-          <h1 className={styles.title}>All Posts</h1>
+          <h1>All Posts</h1>
           <TotalFound total={totalPosts} />
         </header>
         <Grid>
           {Array.isArray(posts) &&
             posts.map((post) => (
-              <li key={post.ID}>
-                <Post post={post} aspect="square" />
-              </li>
+              <Post key={post.ID} post={post} image={post.images.medium_large} aspect="square" />
             ))}
         </Grid>
         <Pagination totalPages={totalPages} currentPage={currentPage} />
