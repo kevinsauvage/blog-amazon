7ce14dbffe3875ad28c1161dc3b601f0721ccd29
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-05-31 19:41:18 +0200

    add middleware and basic auth

---
+++ src/middleware.js
@@ -0,0 +1,27 @@
+/* eslint-disable unicorn/no-null */
+import { NextResponse } from 'next/server';
+
+const middleware = (request) => {
+  const basicAuth = request.headers.get('Authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    // atob is deprecated but Buffer.from is not available in Next.js edge.
+    const [user, password] = atob(authValue).split(':');
+
+    if (user === 'kevin' && password === '5062') {
+      return NextResponse.next();
+    }
+
+    return NextResponse.json(
+      { error: 'Invalid credentials' },
+      { headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' }, status: 401 }
+    );
+  }
+  return NextResponse.json(
+    { error: 'Please enter credentials' },
+    { headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' }, status: 401 }
+  );
+};
+
+export default middleware;
