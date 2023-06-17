12c41f52f8e33ee44f7a5754e26396d4984f9537
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-06-17 18:04:59 +0200

    feat: articles sorting

--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -1,13 +1,16 @@
 import fetchStrapiEndpoint, { formatPosts } from './utils';
 
+// sort : publishedAt:DESC
 const getPosts = async (properties) => {
-  const { perPage = 10, page = 1, category, query, populate = true, slug } = properties || {};
+  const { perPage = 10, page = 1, category, query, populate = true, slug, sort } = properties || {};
 
   let URL = `articles?pagination[pageSize]=${perPage}&pagination[page]=${page}`;
   if (populate) URL += `&populate=*`;
+
   if (category) URL += `&filters[categories][slug][$eqi]=${category}`;
   if (slug) URL += `&filters[slug][$eqi]=${slug}`;
-  if (query) URL += `&filters[title][$contains]=${query}`;
+  if (query) URL += `&filters[title][$$containsi]=${query}`;
+  if (sort) URL += `&sort=${sort}`;
 
   const { data, meta } = (await fetchStrapiEndpoint(URL)) || {};
 
