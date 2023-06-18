9655d10ea99793bb24c3b3a46543a26966b39c58
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-06-18 22:25:25 +0200

    fix space

--- src/components/Views/Views.js
+++ src/components/Views/Views.js
@@ -2,13 +2,15 @@ import IconEyeOutline from '@/svg/IconEyeOutline';
 
 import styles from './Views.module.scss';
 
-const Views = ({ views, className, variant }) => {
+const Views = ({ views = '0', className, variant }) => {
+  console.log('🚀 ~  file: Views.js:7 ~  Views ~  views:', views);
+
   const getStyles = () => {
     if (variant === 'light') return styles.light;
   };
   return (
     <span className={`${styles.views} ${className} ${getStyles()}`}>
-      <IconEyeOutline /> {views} {views > 1 ? 'Views' : 'View'}
+      <IconEyeOutline /> &nbsp;{views === null ? 0 : views} {views > 1 ? 'Views' : 'View'}
     </span>
   );
 };
