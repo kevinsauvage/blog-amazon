02d5a135a642743c79a29ee634dffa8e34773ba8
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-06-23 23:51:11 +0200

    fix

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,23 +1,37 @@
 import apiCalls from '@/lib/api/index';
 
-const { getPosts } = apiCalls;
+const { getPosts, getCategories } = apiCalls;
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default async function sitemap() {
   const postsResponse = await getPosts({ perPage: 100 });
 
-  const posts = postsResponse?.posts?.map(({ slug, date, categories }) => ({
-    lastModified: date,
+  const categoriesResponse = await getCategories();
+
+  const categoriesPath = categoriesResponse.map((category) => ({
+    lastModified: new Date().toISOString(),
+    url: `${BASE_URL}/posts/${category?.slug}`,
+  }));
+
+  const posts = postsResponse?.posts?.map(({ slug, updatedAt, categories }) => ({
+    lastModified: updatedAt,
     url: `${BASE_URL}/posts/${categories?.[0]?.slug}/${slug}`,
   }));
 
-  const routes = ['', '/about', '/contact', '/search', '/terms', '/privacy', '/cookie'].map(
-    (route) => ({
-      lastModified: new Date().toISOString(),
-      url: `${BASE_URL}${route}`,
-    })
-  );
+  const routes = [
+    '',
+    '/about',
+    '/contact',
+    '/search',
+    '/terms',
+    '/privacy',
+    '/cookie',
+    '/posts',
+  ].map((route) => ({
+    lastModified: new Date().toISOString(),
+    url: `${BASE_URL}${route}`,
+  }));
 
-  return [...routes, ...posts];
+  return [...routes, ...categoriesPath, ...posts];
 }
