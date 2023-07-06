f1c2e3f42fa86e90ddf84d62688a77169bc4e9e4
Sauvage kevin <64160579+kevinsauvage@users.noreply.github.com>
2023-07-06 22:08:43 +0200

    fix

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,3 @@
-import { useRef } from 'react';
-
-import useOnClickOutside from '@/hooks/useOnClickOutside';
 import IconClose from '@/svg/IconClose';
 
 import Container from '../Container/Container';
@@ -8,36 +5,37 @@ import Nav from '../Nav/Nav';
 
 import styles from './Menu.module.scss';
 
-const DropdownMenu = ({ menu, usefullLinks, handleClose, show }) => {
-  const menuReference = useRef();
+const Menu = ({ menu, usefullLinks, handleClose, show }) => (
+  <div className={`${styles.menu} ${show ? styles.active : ''}`}>
+    <div
+      onClick={() => handleClose()}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(event) => event.key === 'Enter' && handleClose()}
+      className={styles.overlay}
+    />
+    <div className={styles.content}>
+      <div className={styles.top}>
+        <Container>
+          <button type="button" className={styles.close} onClick={handleClose}>
+            <IconClose />
+            <p>Close</p>
+          </button>
+        </Container>
+      </div>
+      <div className={styles.mainMenu}>
+        <Container>
+          <Nav title={menu?.displayedTitle || 'Menu'} variant="column" menu={menu?.items} />
 
-  useOnClickOutside(menuReference, handleClose);
-
-  return (
-    <div className={`${styles.menu} ${show ? styles.active : ''}`} ref={menuReference}>
-      <div className={styles.content}>
-        <div className={styles.top}>
-          <Container>
-            <button type="button" className={styles.close} onClick={handleClose}>
-              <IconClose />
-              <p>Close</p>
-            </button>
-          </Container>
-        </div>
-        <div className={styles.mainMenu}>
-          <Container>
-            <Nav title={menu?.displayedTitle || 'Menu'} variant="column" menu={menu?.items} />
-
-            <Nav
-              title={usefullLinks?.displayedTitle || 'Menu'}
-              variant="column"
-              menu={usefullLinks?.items}
-            />
-          </Container>
-        </div>
+          <Nav
+            title={usefullLinks?.displayedTitle || 'Menu'}
+            variant="column"
+            menu={usefullLinks?.items}
+          />
+        </Container>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-export default DropdownMenu;
+export default Menu;
